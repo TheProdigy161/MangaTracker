fe0095b06aa98891f047dd302023dcf13ac0323b
Ross Bratton <theprodigy161@yahoo.co.uk>
2022-08-19 16:12:29 +0100

    Updated update service to add automatic update checks

--- src/services/update.service.ts
+++ src/services/update.service.ts
@@ -1,11 +1,14 @@
-import { Injectable } from '@angular/core';
+import { ApplicationRef, Injectable } from '@angular/core';
 import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
-import { filter, map } from 'rxjs';
+import { filter, interval, map } from 'rxjs';
+
+const UPDATE_INTERAL_HOURS: number = 1;
+
 @Injectable({
   providedIn: 'root'
 })
 export class UpdateService {
-  constructor(private swUpdate: SwUpdate) {
+  constructor(private swUpdate: SwUpdate, private appRef: ApplicationRef) {
     const updatesAvailable = swUpdate.versionUpdates.pipe(
       filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
       map(evt => ({
@@ -20,7 +23,16 @@ export class UpdateService {
       }
     });
   }
+
   checkForUpdates() {
-    this.swUpdate.checkForUpdate();
+    this.appRef.isStable.subscribe((isStable: boolean) => {
+      if (isStable) {
+        const timeInterval = interval(UPDATE_INTERAL_HOURS * 60 * 60 * 1000);
+
+        timeInterval.subscribe(() => {
+          this.swUpdate.checkForUpdate();
+        });
+      }
+    });
   }
 }
