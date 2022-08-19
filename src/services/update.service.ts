94cc511cca931756f628d7a731b0ce9e947829ac
Ross Bratton <theprodigy161@yahoo.co.uk>
2022-08-19 15:49:54 +0100

    Updated update service to instead filter versions to newest instead of going through all

--- src/services/update.service.ts
+++ src/services/update.service.ts
@@ -1,11 +1,20 @@
 import { Injectable } from '@angular/core';
-import { SwUpdate } from '@angular/service-worker';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter, map } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class UpdateService {
   constructor(private swUpdate: SwUpdate) {
-    swUpdate.versionUpdates.subscribe(event => {
+    const updatesAvailable = swUpdate.versionUpdates.pipe(
+      filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
+      map(evt => ({
+        type: 'UPDATE_AVAILABLE',
+        current: evt.currentVersion,
+        available: evt.latestVersion,
+      })));
+      
+    updatesAvailable.subscribe(event => {
       if (window.confirm(`A new update is available. Would you like to load the latest version?`)) {
         swUpdate.activateUpdate().then(() => document.location.reload());
       }
