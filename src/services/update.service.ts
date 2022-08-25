ce91f63142ea5074d3c98d6c3207812cf3ea0ff0
Ross Bratton <ross.bratton@yahoo.co.uk>
2022-08-25 23:26:38 +0100

    Updated to add more logging for SwUpdate

--- src/services/update.service.ts
+++ src/services/update.service.ts
@@ -14,6 +14,21 @@ export class UpdateService {
 
     console.log("Constructing UpdateService.");
 
+    swUpdate.versionUpdates.subscribe(evt => {
+      switch (evt.type) {
+        case 'VERSION_DETECTED':
+          console.log(`Downloading new app version: ${evt.version.hash}`);
+          break;
+        case 'VERSION_READY':
+          console.log(`Current app version: ${evt.currentVersion.hash}`);
+          console.log(`New app version ready for use: ${evt.latestVersion.hash}`);
+          break;
+        case 'VERSION_INSTALLATION_FAILED':
+          console.log(`Failed to install app version '${evt.version.hash}': ${evt.error}`);
+          break;
+      }
+    });
+
     const updatesAvailable = swUpdate.versionUpdates.pipe(
       filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY'),
       map(evt => ({
